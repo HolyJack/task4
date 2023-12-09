7ebcf81b0acab4615eb215f3c272b581604af72c
holyjack <holyjackdev@gmail.com>
2023-12-10 01:58:22 +0400

    new domain name for back

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://task4-iota-five.vercel.app";
+axios.defaults.baseURL = "task4-express-back.vercel.app";
 axios.defaults.withCredentials = true;
 
 export default axios;
