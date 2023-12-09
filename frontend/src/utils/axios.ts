e78aef1fa37f45926068cb265aea1a8e51c5de18
holyjack <holyjackdev@gmail.com>
2023-12-10 01:05:46 +0400

    wpw

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://task4-iota-five.vercel.app";
+axios.defaults.baseURL = "https://task4-iota-five.vercel.app";
 axios.defaults.withCredentials = true;
 
 export default axios;
