7433fffd478e3548e3ece8c16d7595e5e7647e99
holyjack <holyjackdev@gmail.com>
2023-12-10 02:05:53 +0400

    possible fix?

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "task4-express-back.vercel.app";
+axios.defaults.baseURL = "https://task4-express-back.vercel.app";
 axios.defaults.withCredentials = true;
 
 export default axios;
