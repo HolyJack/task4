48c227764c2b9dea1bddc9022858917ea31d5208
holyjack <holyjackdev@gmail.com>
2023-12-10 01:04:04 +0400

    wow

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "http://task4-iota-five.vercel.app";
 axios.defaults.withCredentials = true;
 
 export default axios;
