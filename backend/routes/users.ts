d7b80fd3a31ff893e9f4452c4992d98c9635d062
holyjack <holyjackdev@gmail.com>
2023-12-11 22:07:28 +0400

    added post proccessing middleware

--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -7,7 +7,7 @@ const users = express.Router();
 users.use(checkActiveStatus);
 users.use(authenticateStatus);
 
-users.get("/users", async (_, res) => {
+users.get("/users", async (_, res, next) => {
   const users = await prisma.user.findMany({
     select: {
       username: true,
@@ -16,25 +16,30 @@ users.get("/users", async (_, res) => {
       active: true,
     },
   });
-  res.send(users);
+  res.json(users);
+  return next();
 });
 
-users.patch("/users", async (req, res) => {
+users.patch("/users", async (req, res, next) => {
   const usernames = req.body.data.usernames;
   const active = req.body.data.active;
   await prisma.user.updateMany({
     where: { username: { in: usernames } },
     data: { active },
   });
-  res.status(204).send();
+  res.status(204);
+  return next();
 });
 
-users.delete("/users", async (req, res) => {
+users.delete("/users", async (req, res, next) => {
   const usernames = req.body.usernames;
   await prisma.user.deleteMany({
     where: { username: { in: usernames } },
   });
-  res.status(204).send();
+  res.status(204);
+  return next();
 });
+
+users.use(checkActiveStatus);
 
 export { users };
